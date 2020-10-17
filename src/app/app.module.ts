df92cd3a3ba46520488f29818488658d43d30bb8
Zackyy1 <qwertyuk01@gmail.com>
2020-10-17 21:09:41 +0300

    fix for double plant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { GardenComponent } from './layout/garden/garden.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { PlantComponent } from './layout/plant/plant/plant.component';
+import { PlantComponent } from './layout/plant/plant.component';
 import { CreatePlantFormComponent } from './layout/forms/create-plant-form/create-plant-form.component';
 import { EditPlantFormComponent } from './layout/forms/edit-plant-form/edit-plant-form.component';
 import { PopupComponent } from './layout/popup/popup.component';

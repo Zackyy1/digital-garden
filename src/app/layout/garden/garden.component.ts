cbcde9eae2b958bc62684d72633d40d3a42bf5c9
Zackyy1 <qwertyuk01@gmail.com>
2020-10-18 23:51:26 +0300

    cleanup

--- src/app/layout/garden/garden.component.ts
+++ src/app/layout/garden/garden.component.ts
@@ -32,66 +32,11 @@ export class GardenComponent implements OnInit {
     })
 
     $(this.general.POPUP).on('click', e => {
-      // console.log($(e.target), $(e.target).is(POPUP_CONTENT))
       if (!$(e.target).parents(this.general.POPUP_CONTENT).length) {
         this.general.hidePopup();
       }
 
     })
-
-    // $(this.NEW_PLANT_BUTTON).on('click', e => {
-    //   let newPlantForm = `
-        
-    //     `
-    //   let newPlantContent: JQuery<HTMLElement> =
-    //     $(newPlantForm)
-    //   this.general.openPopup(newPlantContent);
-    //   newPlantContent.find('#color-picker').on('click', e => {
-    //     e.preventDefault();
-    //     this.general.openTopLayerPopup($(this.general.COLOR_PICKER_HTML));
-    //   })
-    //   newPlantContent.on('submit', e => {
-    //     e.preventDefault();
-    //     const data = {
-    //       name: newPlantContent.find('#name').val(),
-    //       desc: newPlantContent.find('#desc').val(),
-    //       longDesc: newPlantContent.find('#longDesc').val(),
-    //       level: newPlantContent.find('#level').val(),
-    //     }
-    //     console.log('We gotem', data)
-    //     this.addNewPlant(data);
-    //     this.general.closePopup();
-    //   });
-
-
-    // })
   }
-
-  addNewPlant = (data) => {
-
-
-  }
-  showPlantDetails = (el: JQuery<HTMLElement>) => {
-    const PATH = el.data('path');
-    const PLANT_NAME: string = PATH.substring(PATH.lastIndexOf('/'))
-    // this.general.firestore.collection('plants').doc(PLANT_NAME).get()
-    //   .then(ref => {
-    //     const DATA = ref.data();
-    //     console.log(DATA)
-    //     const POPUP_HTML: string = `
-    //     <div class="plant-popup-content">
-    //         <h1 class="plant-popup-name">${DATA.name}</h1>
-    //         <p class="plant-popup-description">${DATA.longDesc}</p>
-    //         <!-- <div class="images">(todo)</div> -->
-    //         <p class="plant-popup-level">${DATA.level}</p>
-    //     </div>
-    //     `;
-
-    //     this.general.openPopup($(POPUP_HTML)); // Open with DATA
-    //   }).catch(err => {
-    //     console.warn(err);
-    //   });
-  }
-
 
 }
